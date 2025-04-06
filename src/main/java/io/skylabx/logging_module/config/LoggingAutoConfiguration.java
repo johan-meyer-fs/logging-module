215679f217613692eaa72838acfd3e2824a6dddf
Johan Meyer <35699550+JPM72@users.noreply.github.com>
2025-04-07 00:05:50 +0200

    feat: Update LoggingAutoConfiguration to enable property configuration and logger creation

--- src/main/java/io/skylabx/logging_module/config/LoggingAutoConfiguration.java
+++ src/main/java/io/skylabx/logging_module/config/LoggingAutoConfiguration.java
@@ -1,25 +1,32 @@
 package io.skylabx.logging_module.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(LoggingProperties.class)
-@ConditionalOnClass(Slf4j.class)
+// @EnableConfigurationProperties(LoggingProperties.class)
 // @ConditionalOnProperty(name = "logging", havingValue = "true", matchIfMissing = true)
 public class LoggingAutoConfiguration
 {
 	// This class primarily exists to register the properties and enable
 	// auto-configuration
 	// The actual logging configuration happens in logback-spring.xml
+	// private final LoggingProperties properties;
+	// public LoggingAutoConfiguration(LoggingProperties properties)
+	// {
+	// 	System.out.println("Running LoggingAutoConfiguration constructor");
+	// 	this.properties = properties;
+	// }
 
-	private final LoggingProperties properties;
-
-	public LoggingAutoConfiguration(LoggingProperties properties)
-	{
-		System.out.println("Running LoggingAutoConfiguration constructor");
-		this.properties = properties;
-	}
+	@Bean
+    public Logger logger() {
+		System.out.println("Creating logger");
+        return (Logger) LoggerFactory.getLogger("CustomLogger");
+    }
 }
