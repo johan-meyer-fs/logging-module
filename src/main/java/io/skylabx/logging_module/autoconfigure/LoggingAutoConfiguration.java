32dd78c9ee6cb6d78ce1937ec027fef0690d9f36
Johan Meyer <35699550+JPM72@users.noreply.github.com>
2025-04-07 01:31:08 +0200

    xzdddddddddd

--- src/main/java/io/skylabx/logging_module/autoconfigure/LoggingAutoConfiguration.java
+++ src/main/java/io/skylabx/logging_module/autoconfigure/LoggingAutoConfiguration.java
@@ -2,6 +2,7 @@ package io.skylabx.logging_module.autoconfigure;
 
 import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -9,8 +10,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConditionalOnClass(Logger.class)
+@AutoConfiguration
+// @ConditionalOnClass(Logger.class)
 // @EnableConfigurationProperties(LoggingProperties.class)
 // @ConditionalOnProperty(name = "logging", havingValue = "true", matchIfMissing = true)
 public class LoggingAutoConfiguration
@@ -18,17 +19,10 @@ public class LoggingAutoConfiguration
 	// This class primarily exists to register the properties and enable
 	// auto-configuration
 	// The actual logging configuration happens in logback-spring.xml
-	// private final LoggingProperties properties;
-	// public LoggingAutoConfiguration(LoggingProperties properties)
-	// {
-	// 	System.out.println("Running LoggingAutoConfiguration constructor");
-	// 	this.properties = properties;
-	// }
-
-	@Bean
-	@ConditionalOnMissingBean
-    public Logger logger() {
-		System.out.println("Creating logger");
-        return (Logger) LoggerFactory.getLogger("CustomLogger");
-    }
+	private final LoggingProperties properties;
+	public LoggingAutoConfiguration(LoggingProperties properties)
+	{
+		System.out.println("Running LoggingAutoConfiguration constructor");
+		this.properties = properties;
+	}
 }
